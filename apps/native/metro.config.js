7ba1650eeba92d323f6362af382de892aee29b40
LGJ <1752104296@qq.com>
2026-03-04 02:45:04 +0000

    feat:"修复babel-preset-expo与expo-router的问题，架构究极逆天BUG被攻克，耗时近一周"

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,3 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { getWatchFolders } = require('@expo/metro-config/build/getWatchFolders');
+const { getModulesPaths } = require('@expo/metro-config/build/getModulesPaths');
 
-module.exports = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+// 使用 Expo 官方工具自动解析 monorepo watchFolders 和 nodeModulesPaths
+config.watchFolders = getWatchFolders(projectRoot);
+config.resolver.nodeModulesPaths = getModulesPaths(projectRoot);
+
+module.exports = config;
